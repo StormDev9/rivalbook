c50fdc5b9d852221f6c29f23ab7ca18270c65b40
StormDev9 <stormclouddevelopment@gmail.com>
2026-04-07 12:55:57 +0100

    testing

--- SRC/boot.ts
+++ SRC/boot.ts
@@ -1,9 +1,12 @@
 import { BrowserWindow, app } from "electron";
 
 import { PROJECT } from "./main.js";
+import updater from "electron-updater";
 
 console.log(PROJECT.root);
 app.on("ready", async () => {
     const win = new BrowserWindow();
     win.loadFile(PROJECT.getFilePath("test.html"));
+
+    updater.autoUpdater.checkForUpdatesAndNotify();
 });
