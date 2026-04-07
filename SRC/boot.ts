ac1ed36b62951f5bad626de192ac01f9795889e7
StormDev9 <stormclouddevelopment@gmail.com>
2026-04-07 23:22:46 +0100

    added server foundations

--- SRC/boot.ts
+++ SRC/boot.ts
@@ -1,18 +1,12 @@
 import { BrowserWindow, app } from "electron";
 
 import { PROJECT } from "./main.js";
+import { startServer } from "./SYSTEM/server/router.js";
 import updateLib from "electron-updater";
 
 const { autoUpdater } = updateLib;
 
 console.log(PROJECT.root);
 app.on("ready", async () => {
-    const win = new BrowserWindow();
-    win.loadFile(PROJECT.getFilePath("test.html"));
-
-    autoUpdater.checkForUpdatesAndNotify();
-
-    autoUpdater.on("update-downloaded", (info) => {
-        autoUpdater.quitAndInstall(); // forces install
-    });
+    await startServer();
 });
